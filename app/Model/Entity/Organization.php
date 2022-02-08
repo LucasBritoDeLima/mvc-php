e5d1c4ba9a29100fdd88ea22fe38462118ece4e0
= <lucasbritocientista@gmail.com>
2022-02-07 22:58:50 -0300

    MVC em PHP: Conceito e início do projeto - Série MVC em PHP - Parte 1

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization{
+
+  /**
+   * ID da organização
+   * @var integer
+   */
+  public $id = 1;
+
+  /**
+   * Nome da organização
+   * @var string
+   */
+  public $name = 'Canal WDEV';
+
+  /**
+   * Site da organização
+   * @var string
+   */
+  public $site = 'https://www.youtube.com/c/WDEVoficial';
+
+  /**
+   * Descrição da organização
+   * @var string
+   */
+  public $description = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo laboriosam voluptatum odio, placeat, nihil corporis accusamus dolor ipsum autem aut quasi tenetur, a debitis necessitatibus quam sunt adipisci aliquid! Nobis.';
+}
